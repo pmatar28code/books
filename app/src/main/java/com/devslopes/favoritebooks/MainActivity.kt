6723841a40d6a12c252e49c2a2b7611a0420127d
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-02-06 06:45:29 -0600

    swipe left right working. still need to delete
    swipe left right working. still need to delete.

--- app/src/main/java/com/devslopes/favoritebooks/MainActivity.kt
+++ app/src/main/java/com/devslopes/favoritebooks/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.devslopes.favoritebooks
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.devslopes.favoritebooks.databinding.ActivityMainBinding
 import com.devslopes.favoritebooks.models.Book
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.book_list
-import kotlinx.android.synthetic.main.activity_main.view.*
-import java.util.jar.Attributes
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +23,30 @@ class MainActivity : AppCompatActivity() {
         binding.bookList.apply {
             adapter = BooksAdapter(BookRepository.getBooks(this@MainActivity))
 
+            val mIth = ItemTouchHelper(
+                    object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+                            ItemTouchHelper.UP) {
+                        override fun onMove(recyclerView: RecyclerView,
+                                                     viewHolder: ViewHolder, target: ViewHolder): Boolean {
+                            val fromPos = viewHolder.adapterPosition
+                            val toPos = target.adapterPosition
+                            // move item in `fromPos` to `toPos` in adapter.
+                            return true // true if moved, false otherwise
+                        }
 
+                        override fun onSwiped(viewHolder: ViewHolder, direction: Int ) {
+                            // remove from adapter
 
+                             //var position: Int = viewHolder.adapterPosition
+                           // BookRepository.removeBook()
 
+                           // var t = BookRepository.getBooks(this@MainActivity)[getChildAdapterPosition(binding.bookList.get())]
+                            //BookRepository.removeBook(t,this@MainActivity)
+
+                        }
+                    })
+
+                        mIth.attachToRecyclerView(this)
 
             layoutManager = LinearLayoutManager(this@MainActivity,
                     LinearLayoutManager.VERTICAL,
